6af023b06cd2015db7102e171d6a9ad2427ed72b
morera <arthhmoreira99@gmail.com>
2024-10-16 18:24:50 -0300

    Ajustes finais

--- src/test/java/br/com/arthur/petstore/model/Category.java
+++
@@ -1,6 +0,0 @@
-package br.com.arthur.petstore.model;
-
-public class Category {
-    int id;
-    String name;
-}
--- src/test/java/br/com/arthur/petstore/model/Tag.java
+++
@@ -1,6 +0,0 @@
-package br.com.arthur.petstore.model;
-
-public class Tag {
-    int id;
-    String name;
-}
